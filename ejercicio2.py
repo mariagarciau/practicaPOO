633a7dfd618c886c34d4310257175cd725520ac7
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-13 23:52:27 +0100

    cosas varias

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,3 +3,14 @@
 y añadir los métodos de gestión que permitan listar los elementos uno a uno,
 en los dos sentidos, añadir un elemento, eliminarlo y volver al primero?
 """
+miLista = [8,6,23,50,20,9,10,2,1,7,5]
+print(miLista)
+miLista.append(4)
+print(miLista)
+miLista.remove(6)
+print(miLista)
+print(max(miLista))
+print(min(miLista))
+print(len(miLista))
+miLista.sort()
+print(miLista)
