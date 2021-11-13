4d251dcd3a35e8eafaa133960af0db461c533663
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-13 21:58:18 +0100

    No funciona:)

--- ejercicio4.py
+++ ejercicio4.py
@@ -26,11 +26,11 @@ class CuentaBancaria():
     def __init__(self,ID,titular,fechaApertura,nºcuenta,saldo):
         str : self.ID = ID
         str : self.titular = titular
-        fechaApertura = input("Intorduce la fecha en la que quiere retirar el dinero: "+date())
+        fechaApertura = date(2020,11,6)
         int : self.nºcuenta = nºcuenta
         float: self.saldo = saldo
     def retirarDinero(self, dineroRetirado):
-        if dineroRetirado>self.saldo:
+        if self.saldo<dineroRetirado:
             print("No dispone del dinero suficiente para retirar la cantidad que desea")
         else:
             self.saldo = self.saldo - dineroRetirado
@@ -39,7 +39,7 @@ class CuentaBancaria():
         self.saldo = self.saldo + dineroIngresado
         print("Ha ingresado: "+str(dineroIngresado)+"€. Su saldo actual es de: "+str(self.saldo)+"€")
     def transferirDinero(self, dineroTransferido):
-        if dineroTransferido>self.saldo:
+        if self.saldo<dineroTransferido:
             print("No dispone del dinero suficiente para transferir la cantidad que desea")
         else:
             self.saldo = self.saldo - dineroTransferido
@@ -48,7 +48,7 @@ class CuentaPlazoFijo():
     def __init__(self,ID,titular,fechaApertura,nºcuenta,saldo):
         str : self.ID = ID
         str : self.titular = titular
-        str : self.fechaApertura = fechaApertura
+        fechaApertura = date(2020,11,6)
         int : self.nºcuenta = nºcuenta
         float: self.saldo = saldo
 
@@ -77,7 +77,7 @@ class CuentaVIP():
     def __init__(self,ID,titular,fechaApertura,nºcuenta,saldo,negativoMax):
         str : self.ID = ID
         str : self.titular = titular
-        str : self.fechaApertura = fechaApertura
+        fechaApertura = date(2020,11,6)
         int : self.nºcuenta = nºcuenta
         float: self.saldo = saldo
         float : self.negativoMax = negativoMax
@@ -97,22 +97,22 @@ class CuentaVIP():
             self.saldo = self.saldo - dineroTransferido
             print("Ha transferido: "+str(dineroTransferido)+"€. Su saldo actual es de: "+str(self.saldo)+"€")
 class lasTresCuentas(CuentaBancaria,CuentaPlazoFijo,CuentaVIP):
-    cuenta1 = CuentaBancaria("ES100200300","María",date(),random.randint(1,15),10000)
-    cuenta2 = CuentaPlazoFijo("ES200300400","Sara",date(),random.randint(1,15),5000)
-    cuenta3 = CuentaVIP("ES300400500","Paula",date(),random.randint(1,12),2000)
+    cuenta1 = CuentaBancaria("ES100200300","María",date(2021,3,19),random.randint(1,15),10000)
+    cuenta2 = CuentaPlazoFijo("ES200300400","Sara",date(2021,1,12),random.randint(1,15),5000)
+    cuenta3 = CuentaVIP("ES300400500","Paula",date(2021,3,6),random.randint(1,12),2000,3000)
     if cuenta1.saldo()>=10000:
         cuenta1.transferirDinero(2000)
         cuenta1.retirarDinero(78)
         cuenta1.ingresarDinero(575)
         cuenta2.saldo=cuenta2.saldo+2000
         cuenta3.saldo=cuenta3.saldo+2000
-    if cuenta2.saldo()>=10000:
+    if cuenta2.saldo>=10000:
         cuenta2.transferirDinero(2000)
         cuenta2.retirarDinero(78)
         cuenta2.ingresarDinero(575)
         cuenta1.saldo=cuenta1.saldo+2000
         cuenta3.saldo=cuenta3.saldo+2000
-    if cuenta3.saldo()>=10000:
+    if cuenta3.saldo>=10000:
         cuenta3.transferirDinero(2000)
         cuenta3.retirarDinero(78)
         cuenta3.ingresarDinero(575)
