13b818abe0d4dff5b6347204ae816d638e446b84
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-13 23:03:12 +0100

    libro bebi

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,4 +2,12 @@
 Dar un ejemplo de uso en Python.
 """
 class Libro():
-    pass
+    def __init__(self,titulo,nºpaginas,autor,editorial,fechaPublicacion,nºediciones):
+        str:self.titulo = titulo
+        int:self.nºpaginas = nºpaginas
+        str:self.autor = autor
+        str:self.editorial = editorial
+        str:self.fechaPublicacion = fechaPublicacion
+        int:self.nºediciones = nºediciones
+libro1=Libro("Memorias de una salvaje",464,"Bebi Fernandez","Planeta","Noviembre 2018",10)
+
